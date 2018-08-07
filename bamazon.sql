3a2a1d19368749e589e3c7df41f2a6a45cebd10e
giacomo2398@gmail.com <g701f520@ku.edu>
2018-08-06 20:04:34 -0500

    Quick fix sql file

--- bamazon.sql
+++ bamazon.sql
@@ -4,14 +4,14 @@ CREATE DATABASE bamazon;
 USE bamazon;
 
 CREATE TABLE products(
-    id INTEGER(10) AUTO_INCREMENT NOT NULL,
-    Product_name VARCHAR(32) NOT NULL,
-    Department_name VARCHAR(32) NOW NULL,
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    Product_name VARCHAR(100) NOT NULL,
+    Department_name VARCHAR(32) NOT NULL,
     Price INTEGER(10) NOT NULL,
     Stock_quantity INTEGER(10) NOT NULL
 );
 
-INSERT INTO products (Product_name, Department_name, Price, Stock_quantity)
+INSERT INTO products (id, Product_name, Department_name, Price, Stock_quantity)
 VALUES(1, "Arizona Green Tea w/ Gingseng", "Grocery & Gourmet Food",7, 77),
 (2, "COSORI 8 Quart 8-in-1 Multi-Functional Programmable Pressure Cooker, Slow Cooker, Rice Cooker", "Home & Kitchen",80, 40),
 (3, "Orgain Organic Protein", "Grocery & Gourmet Food",31, 1),
@@ -28,22 +28,22 @@ CREATE TABLE users(
 	UserID INT NOT NULL AUTO_INCREMENT,
     Email VARCHAR(32) NOT NULL,
     Password CHAR(32) NOT NULL,
-    Username VARCHAR(32)
+    Username VARCHAR(32),
     Orders INTEGER(32),
     primary key(UserID)
 );
 
-INSERT INTO users(UserID, Email, Password, Orders, Username)
-VALUES( 1,"example@gmail.com", md5("123456789"), 0 ,Database_guy1);
+INSERT INTO users(UserID, Email, Password, Username, Orders)
+VALUES( 1,"example@gmail.com", md5("123456789"), "Databaseguy1", 0);
 
-CREATE TABLE manager(
+CREATE TABLE managers(
 	UserID INT NOT NULL AUTO_INCREMENT,
     Email VARCHAR(80) NOT NULL,
     Password CHAR(32) NOT NULL,
-    Username VARCHAR(50),
+    Username VARCHAR(50) NOT NULL,
     Orders INTEGER(50),
     primary key(UserID)
 );
 
 INSERT INTO managers(UserID, Email, Password, Username, Orders)
-VALUES( 1,"example@gmail.com", md5("123456789"),0 ,Database_guy1);
+VALUES( 1,"example@gmail.com", md5("123456789"), "Databaseguy1" , 0);
